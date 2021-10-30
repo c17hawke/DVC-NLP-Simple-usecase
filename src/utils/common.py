12a6aa34c93be9ff9bc71fe0f506179c27067cfe
c17hawke <sunny.c17hawke@gmail.com>
2021-10-30 08:47:22 +0530

    st - 01 base implimentation

---
+++ src/utils/common.py
@@ -0,0 +1,12 @@
+import os
+import yaml
+import logging
+import time
+import pandas as pd
+import json
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+    return content
