468f98fb43b10e9425c7bfb53e2298176b1c2dfe
c17hawke <sunny.c17hawke@gmail.com>
2021-10-31 09:22:38 +0530

    st -03 train base file created

---
+++ src/stage_03_train.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+
+STAGE = "Template stage"
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+def main(config_path, params_path):
+    pass
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
